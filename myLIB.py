c856620fafdfa49cbde60a5a74adf1e8b653c0c5
IGS <amaanulhaq55@gmail.com>
2024-08-02 18:57:01 +0500

    codebase updated

---
+++ myLIB.py
@@ -0,0 +1,16 @@
+def Total(Lst):
+    cout = 0
+    for i in Lst:
+        cout = cout + i
+    return cout
+    
+def Min(m):
+    minim = min(m)
+    return minim
+
+def Max(m):
+    maxim = max(m)
+    return maxim 
+
+def f2c(fahrenheit):
+    return (fahrenheit - 32) * 5.0/9.0
